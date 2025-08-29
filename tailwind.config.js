c73011e61324d0337193306268badcf99ace2972
Mathaus <67625341+CathausXD@users.noreply.github.com>
2025-08-29 16:49:55 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        piano: "#0a0a0a",
+        cyan: "#06b6d4",
+      },
+    },
+  },
+  plugins: [],
+}
